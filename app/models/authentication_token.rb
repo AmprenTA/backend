9bd6a4574d62ed81da10bffdc5f5ae59a4e4edde
Sergiu <sergiu.lupaiescu@assist.ro>
2022-11-04 19:14:53 +0200

    Generated User model and migration

---
+++ app/models/authentication_token.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AuthenticationToken < ApplicationRecord
+  belongs_to :user
+  validates :token, presence: true
+  scope :valid, -> { where { (expires_at.nil?) | (expires_at > Time.zone.now) } }
+end
