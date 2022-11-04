1bb2b309528538c04a038763f025d9f94f6637e9
Sergiu <sergiu.lupaiescu@assist.ro>
2022-11-04 19:14:14 +0200

    Added login and register functionalities

--- app/api/root_api.rb
+++ app/api/root_api.rb
@@ -7,7 +7,15 @@ class RootApi < Grape::API
     error!('Not Found', 404)
   end
 
-  # mount NotificationsApi
+  rescue_from Grape::Exceptions::ValidationErrors do |e|
+    rack_response({
+      status: e.status,
+      error_msg: e.message
+    }.to_json, 400)
+  end
+
+  mount Login
+  mount Register
 
   add_swagger_documentation(
     format: :json,
