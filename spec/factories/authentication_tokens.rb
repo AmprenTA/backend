5f138f2f3ba6f9f2809f16df93b122efca74e154
Sergiu <sergiu.lupaiescu@assist.ro>
2022-11-15 13:47:42 +0200

    Deleted specs and authentication token model

--- spec/factories/authentication_tokens.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :authentication_token do
-    token { Faker::Crypto.sha256 }
-    user
-    expires_at { 10.hours.from_now }
-  end
-end
--- spec/models/footprint_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe Footprint do
-  it { is_expected.to have_many(:cars) }
-  it { is_expected.to have_many(:flights) }
-  it { is_expected.to have_many(:public_transports) }
-end
