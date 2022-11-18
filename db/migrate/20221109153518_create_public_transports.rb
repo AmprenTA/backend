69535dc19abb526caf708a086b117427d8a74766
Sergiu <sergiu.lupaiescu@assist.ro>
2022-11-18 21:42:31 +0200

    added default value for carbon footprint on migration

--- db/migrate/20221109153518_create_public_transports.rb
+++ db/migrate/20221109153518_create_public_transports.rb
@@ -3,7 +3,7 @@ class CreatePublicTransports < ActiveRecord::Migration[6.1]
     create_table :public_transports do |t|
       t.integer :transport_type, null: false
       t.float :total_km, null: false
-      t.float :carbon_footprint, null: false
+      t.float :carbon_footprint, null: false, default: 0
       t.references :footprint, foreign_key: true
 
       t.timestamps
--- db/migrate/20221109153547_create_cars.rb
+++ db/migrate/20221109153547_create_cars.rb
@@ -4,7 +4,7 @@ class CreateCars < ActiveRecord::Migration[6.1]
       t.integer :fuel_type, null: false
       t.integer :fuel_consumption, null: false
       t.float :total_km, null: false
-      t.float :carbon_footprint, null: false
+      t.float :carbon_footprint, null: false, default: 0
       t.references :footprint, foreign_key: true
 
       t.timestamps
