339529a4085f9bcdf84e676691681bf46ad282b1
Petru G. Cioată <petru.cioata@gmail.com>
2022-11-19 15:03:12 +0200

    Update FoodFootprintCalculator service

--- app/api/foods_api.rb
+++ app/api/foods_api.rb
@@ -8,7 +8,7 @@ class FoodsApi < Grape::API
     desc 'Create foods' do
       tags %w[foods]
       http_codes [
-        { code: 201, message: 'Create foods' },
+        { code: 201, message: 'Foods created!' },
         { code: 400, message: 'Bad request!' }
       ]
     end
@@ -21,31 +21,40 @@ class FoodsApi < Grape::API
       }
     }
     params do
-      requires :beef, type: Array[Integer], desc: 'beef', documentation: { param_type: 'body' }
-      requires :lamb, type: Array[Integer], desc: 'lamb', documentation: { param_type: 'body' }
-      requires :poultry, type: Array[Integer], desc: 'poultry', documentation: { param_type: 'body' }
-      requires :pork, type: Array[Integer], desc: 'pork', documentation: { param_type: 'body' }
-      requires :fish, type: Array[Integer], desc: 'fish', documentation: { param_type: 'body' }
-      requires :milk_based, type: Array[Integer], desc: 'milk_based', documentation: { param_type: 'body' }
-      requires :cheese, type: Array[Integer], desc: 'cheese', documentation: { param_type: 'body' }
-      requires :eggs, type: Array[Integer], desc: 'eggs', documentation: { param_type: 'body' }
-      requires :coffee, type: Array[Integer], desc: 'coffee', documentation: { param_type: 'body' }
-      requires :vegetables, type: Array[Integer], desc: 'vegetables', documentation: { param_type: 'body' }
-      requires :bread, type: Array[Integer], desc: 'bread', documentation: { param_type: 'body' }
+      requires :beef, type: Integer, desc: 'beef', documentation: { param_type: 'body' }
+      requires :lamb, type: Integer, desc: 'lamb', documentation: { param_type: 'body' }
+      requires :poultry, type: Integer, desc: 'poultry', documentation: { param_type: 'body' }
+      requires :pork, type: Integer, desc: 'pork', documentation: { param_type: 'body' }
+      requires :fish, type: Integer, desc: 'fish', documentation: { param_type: 'body' }
+      requires :milk_based, type: Integer, desc: 'milk_based', documentation: { param_type: 'body' }
+      requires :cheese, type: Integer, desc: 'cheese', documentation: { param_type: 'body' }
+      requires :eggs, type: Integer, desc: 'eggs', documentation: { param_type: 'body' }
+      requires :coffee, type: Integer, desc: 'coffee', documentation: { param_type: 'body' }
+      requires :vegetables, type: Integer, desc: 'vegetables', documentation: { param_type: 'body' }
+      requires :bread, type: Integer, desc: 'bread', documentation: { param_type: 'body' }
       requires :footprint_id, type: Integer, desc: 'footprint_id', documentation: { param_type: 'body' }
     end
+
     post do
       token = headers.fetch('Auth-Token', nil)
       authorize_user(token) if token
-      min_max_carbon_footprint = FoodFootprintCalculator.new(params).call
+
+      min_max_carbon_footprint = FoodFootprintCalculator.call(params)
       footprint_id = params[:footprint_id]
+
       food = Food.new(
         min_carbon_footprint: min_max_carbon_footprint[0],
         max_carbon_footprint: min_max_carbon_footprint[1],
         footprint_id:
       )
-      error!(food.errors, 400) unless food.present? && food.save
-      food_carbon_footprint = { min_max_carbon_footprint:, footprint_id: }
+      error!(food.errors, 400) unless food&.save
+
+      food_carbon_footprint = {
+        min_carbon_footprint: min_max_carbon_footprint[0],
+        max_carbon_footprint: min_max_carbon_footprint[1],
+        footprint_id:
+      }
+
       present food_carbon_footprint
     end
   end
--- app/services/food_footprint_calculator.rb
+++ app/services/food_footprint_calculator.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class FoodFootprintCalculator
+  KG = 1000.0 # 1000 g = 1 kg
+
   def initialize(food)
     @food = food
+  end
+
+  def self.call(food)
+    new(food).call
   end
 
   def call
@@ -13,8 +19,32 @@ class FoodFootprintCalculator
 
   attr_reader :food
 
+  def frequency(value)
+    case value
+    when 0
+      [0, 0] # never
+    when 1
+      [1, 2] # once/twice a month
+    when 2
+      [4, 8] # once/twice a week
+    when 3
+      [20, 30] # everyday
+    when 4
+      [60, 90] # multiple times a day
+    end
+  end
+
   def calculate_carbon_footprint
-    carbon_footprint_min_max_list = [
+    carbon_footprint_min_max_list = aggregated_footprints
+
+    [
+      carbon_footprint_min_max_list.sum { |interval| interval[0] },
+      carbon_footprint_min_max_list.sum { |interval| interval[1] }
+    ]
+  end
+
+  def aggregated_footprints
+    [
       beef_carbon_footprint,
       lamb_carbon_footprint,
       poultry_carbon_footprint,
@@ -27,54 +57,60 @@ class FoodFootprintCalculator
       vegetables_carbon_footprint,
       bread_carbon_footprint
     ]
-
-    [
-      carbon_footprint_min_max_list.sum { |interval| interval[0] },
-      carbon_footprint_min_max_list.sum { |interval| interval[1] }
-    ]
   end
 
   def beef_carbon_footprint
-    food.fetch('beef', 0).map { |n| n * (FoodConstants::BEEF * 100 / 1000.0) }
+    beef = food.fetch('beef', 0)
+    frequency(beef).map { |n| n * (FoodConstants::BEEF * 100 / KG) }
   end
 
   def lamb_carbon_footprint
-    food.fetch('lamb', 0).map { |n| n * (FoodConstants::LAMB * 100 / 1000.0) }
+    lamb = food.fetch('lamb', 0)
+    frequency(lamb).map { |n| n * (FoodConstants::LAMB * 100 / KG) }
   end
 
   def poultry_carbon_footprint
-    food.fetch('poultry', 0).map { |n| n * (FoodConstants::POULTRY * 100 / 1000.0) }
+    poultry = food.fetch('poultry', 0)
+    frequency(poultry).map { |n| n * (FoodConstants::POULTRY * 100 / KG) }
   end
 
   def pork_carbon_footprint
-    food.fetch('pork', 0).map { |n| n * (FoodConstants::PORK * 100 / 1000.0) }
+    pork = food.fetch('pork', 0)
+    frequency(pork).map { |n| n * (FoodConstants::PORK * 100 / KG) }
   end
 
   def fish_carbon_footprint
-    food.fetch('fish', 0).map { |n| n * (FoodConstants::FISH * 140 / 1000.0) }
+    fish = food.fetch('fish', 0)
+    frequency(fish).map { |n| n * (FoodConstants::FISH * 140 / KG) }
   end
 
   def milk_based_carbon_footprint
-    food.fetch('milk_based', 0).map { |n| n * (FoodConstants::MILK_BASED * 200 / 1000.0 * 1.03) }
+    milk_based = food.fetch('milk_based', 0)
+    frequency(milk_based).map { |n| n * (FoodConstants::MILK_BASED * 200 / KG * 1.03) }
   end
 
   def cheese_carbon_footprint
-    food.fetch('cheese', 0).map { |n| n * (FoodConstants::CHEESE * 30 / 1000.0) }
+    cheese = food.fetch('cheese', 0)
+    frequency(cheese).map { |n| n * (FoodConstants::CHEESE * 30 / KG) }
   end
 
   def eggs_carbon_footprint
-    food.fetch('eggs', 0).map { |n| n * (FoodConstants::EGGS * 120 / 1000.0) }
+    eggs = food.fetch('eggs', 0)
+    frequency(eggs).map { |n| n * (FoodConstants::EGGS * 120 / KG) }
   end
 
   def coffee_carbon_footprint
-    food.fetch('coffee', 0).map { |n| n * (FoodConstants::COFFEE * 180 / 1000.0) }
+    coffee = food.fetch('coffee', 0)
+    frequency(coffee).map { |n| n * (FoodConstants::COFFEE * 180 / KG) }
   end
 
   def vegetables_carbon_footprint
-    food.fetch('vegetables', 0).map { |n| n * (FoodConstants::VEGETABLES * 80 / 1000.0) }
+    vegerables = food.fetch('vegetables', 0)
+    frequency(vegerables).map { |n| n * (FoodConstants::VEGETABLES * 80 / KG) }
   end
 
   def bread_carbon_footprint
-    food.fetch('bread', 0).map { |n| n * (FoodConstants::BREAD * 36 / 1000.0) }
+    bread = food.fetch('bread', 0)
+    frequency(bread).map { |n| n * (FoodConstants::BREAD * 36 / KG) }
   end
 end
