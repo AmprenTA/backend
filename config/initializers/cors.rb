63990cc94a4b12b84365325da8f876c023f6a5b7
Sergiu <sergiu.lupaiescu@assist.ro>
2022-11-21 09:13:02 +0200

    Updated CORS

---
+++ config/initializers/cors.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # origins are set in the environment files
+    origins Rails.application.config.allowed_cors_origins
+    resource '*', headers: :any, methods: %i[get post put patch head options delete]
+  end
+end
