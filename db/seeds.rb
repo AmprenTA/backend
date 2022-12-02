0b087e63a549bedd94a41014ba95150d485dff92
Petru G. Cioată <petru.cioata@gmail.com>
2022-12-02 12:44:52 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,25 @@
 # frozen_string_literal: true
 
-total_footprint_entries = ENV['total_entries'] || 100
+FLIGHTS_PERCENTAGE_PER_MONTH = 0.0842 # 8.42%
+total_footprint_entries = ENV['total_footprints'].to_i || 1_000
+
+total_flights = (total_footprint_entries * FLIGHTS_PERCENTAGE_PER_MONTH).ceil
+total_cars = (((60..72).to_a.sample * total_footprint_entries)/100).ceil
+total_public_transports = (((25..60).to_a.sample * total_footprint_entries)/100).ceil
 
 puts 'Start seeding...'
+
 0.upto(total_footprint_entries) do |index|
   footprint = Footprint.create
 
   FactoryBot.create(:house, footprint: footprint)
   FactoryBot.create(:food, footprint: footprint)
 
-  FactoryBot.create(:car, footprint: footprint)
-  FactoryBot.create(:flight, footprint: footprint)
-  FactoryBot.create(:public_transport, footprint: footprint)
-  # TODO: update the frequencies for each type
+  FactoryBot.create(:car, footprint: footprint) if index <= total_cars
+
+  FactoryBot.create(:public_transport, footprint: footprint) if index <= total_public_transports
+
+  FactoryBot.create(:flight, footprint: footprint) if index <= total_flights + 1
 end
+
 puts 'Seeding complete!'
