7dbf68a62cff9a625c954ba870dea2ae191eecef
Petru G. Cioată <petru.cioata@gmail.com>
2022-12-06 19:45:43 +0200

    Add locations/counties endpoint

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,53 @@ class Location < ApplicationRecord
   def to_s
     "#{town}, #{county}"
   end
+
+  # rubocop:disable Metrics/MethodLength
+  def self.counties
+    %w[
+      Vrancea
+      Satu Mare
+      Bistriţa-Năsăud
+      Tulcea
+      Dâmboviţa
+      Dolj
+      Caraş-Severin
+      Bihor
+      Prahova
+      Harghita
+      Suceava
+      Mureş
+      Călăraşi
+      Olt
+      Botoşani
+      Buzău
+      Sibiu
+      Gorj
+      Bacău
+      Arad
+      Argeş
+      Neamţ
+      Cluj
+      Vâlcea
+      Constanţa
+      Teleorman
+      Ilfov
+      Ialomiţa
+      Hunedoara
+      Bucureşti
+      Sălaj
+      Giurgiu
+      Mehedinţi
+      Covasna
+      Iaşi
+      Maramureş
+      Braşov
+      Brăila
+      Vaslui
+      Alba
+      Timiş
+      Galaţi
+    ]
+  end
+  # rubocop:enable Metrics/MethodLength
 end
